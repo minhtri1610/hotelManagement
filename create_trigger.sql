09451fd143d64cf4980f6d0e3951684d3c534581
epsminhtri@gmail.com <epsminhtri@gmail.com>
2022-07-22 06:53:06 +0700

    update code

--- create_trigger.sql
+++ create_trigger.sql
@@ -1,8 +1,21 @@
+
 create or replace function fn_service_order_item_insert()
 returns trigger as
 $$
 begin
-    
+    IF EXISTS( SELECT *
+            FROM 
+                SERVICE_ORDER_ITEMS 
+            WHERE 
+                SERVICE_ORDER_ITEMS.create_date = NEW.create_date 
+                AND SERVICE_ORDER_ITEMS.service_id = NEW.service_id
+                AND SERVICE_ORDER_ITEMS.reservation_id = NEW.reservation_id
+            ) THEN
+        
+    ELSE
+
+    END IF;
+
 end;
 $$
 language 'plpgsql'
